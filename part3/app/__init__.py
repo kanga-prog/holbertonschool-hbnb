c63103a5063eff060f4117a07dce27fe3d3f8d1a
kanga-prog <9952@holbertonstudents.com>
2025-03-10 15:45:34 +0100

    Modified the application factory to include configuration management

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -7,6 +7,10 @@ from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
 
+
+# Import the configuration class
+from config import DevelopmentConfig
+
 def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
     
