a1341c395efc37420178b9f78c269cff5aff001b
Nordine000 <ounissi970@gmail.com>
2025-03-14 09:09:25 +0000

    Task 5

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,16 +1,19 @@
 from flask import Flask
 from flask_restx import Api
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
+
 from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
-from config import DevelopmentConfig
-from flask_jwt_extended import JWTManager
 from app.api.v2.auth import api as auth_ns
 from app.api.v2.protected import api as protected_ns
-from flask_bcrypt import Bcrypt
+from config import DevelopmentConfig
 
 import os
+
 
 jwt = JWTManager()
 db = SQLAlchemy()
--- part3/config.py
+++ part3/config.py
@@ -2,19 +2,21 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
-    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'default_jwt_secret_key')  # Clé secrète pour JWT
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'default_jwt_secret_key')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = None
+    DEBUG = False
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///dev.db'  # SQLite pour le développement
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///dev.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class ProductionConfig(Config):
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')  # URL de connexion MySQL en production
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
 
-# Dictionnaire de configuration pour la rendre dynamique
+
 configurations = {
     'development' : DevelopmentConfig,
     'production' : ProductionConfig,
